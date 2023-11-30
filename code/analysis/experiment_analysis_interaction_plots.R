dd7d1476ec3e0cd52836b574f23b48098e779146
Jacob Winch <winch@ualberta.ca>
2023-11-30 15:46:39 -0700

    I did some repository organization

---
+++ code/analysis/experiment_analysis_interaction_plots.R
@@ -0,0 +1,44 @@
+library(car)
+
+distance = c(245, 247, 241,
+             248, 258, 249,
+             247, 232, 240, 
+             246, 212, 226,
+             236, 240, 248,
+             247, 234, 238,
+             192, 249, 253,
+             245, 249, 220,
+             242, 227, 235,
+             259, 248, 231,
+             254, 243, 241,
+             249, 244, 237,
+             264, 265, 249,
+             244, 255, 244,
+             220, 231, 258,
+             221, 249, 253,
+             228, 264, 250,
+             282, 260, 275,
+             251, 240, 255,
+             244, 252, 255,
+             259, 276, 277,
+             264, 287, 275,
+             256, 256, 265,
+             230, 224, 252, 
+             241, 255, 254,
+             260, 266, 264,
+             261, 255, 275)
+
+driver = as.factor(rep(c("R7", "M5", "Stealth 2 Plus"), each = 27))
+
+golf_ball = as.factor(rep(c("Soft Response", "Tour Response", "TP5"), each = 9, times = 3))
+
+golf_ball_number = as.factor(rep(c(1:3), times = 9, each = 3))
+
+model = lm(distance~(driver + golf_ball)^2 + 
+             (golf_ball_number + driver*golf_ball_number)%in%golf_ball)
+
+interaction.plot(driver, golf_ball, distance, col = c(3,4))
+interaction.plot(golf_ball, golf_ball_number, distance, col = c(3,4))
+interaction.plot(driver, golf_ball_number, distance, col = c(3,4))
+
+
